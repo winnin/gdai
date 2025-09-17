463aca0a754057515ecd65fa47be64438f9fd657
firmino <firminodefaria@gmail.com>
2025-09-17 08:54:14 -0300

    task: change workflow name patterns

--- gdai/repositories/sqlalchemy.py
+++ gdai/repositories/sqlalchemy.py
@@ -12,7 +12,7 @@ min_size = Config.db.PGVECTOR_MIN_POOL_CONNECTIONS
 max_size = Config.db.PGVECTOR_MAX_POOL_CONNECTIONS
 
 
-DATABASE_URL = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}"
+DATABASE_URL = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}"  # TODO: change to accept any possible database.
 
 engine = create_async_engine(DATABASE_URL, echo=False, pool_size=min_size, max_overflow=max_size - min_size)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
